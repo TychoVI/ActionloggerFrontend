29ead15f40871e2d8fc0a8dbecb13a5da2897537
TychoVI <tycho@vanisterdael.email>
2022-05-30 13:12:53 +0200

    Add bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,37 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import mainOidc from "@/oidc";
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
 </script>
 
 <template>
   <header>
     <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/actions">Actions</RouterLink>
-        <a href="#" @click="mainOidc.signIn()">SignIn</a>
-        <a href="#" @click="mainOidc.signOut()">SignOut</a>
+      <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Actionlogger</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link"><RouterLink to="/">Home</RouterLink></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link"><RouterLink to="/about">About</RouterLink></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link"><RouterLink to="/actions">Actions</RouterLink></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" v-if="mainOidc.isAuthenticated"  href="#" @click="mainOidc.signOut()">SignOut</a>
+                <a class="nav-link" v-else href="#" @click="mainOidc.signIn()">SignIn</a>
+              </li>
+            </ul>
+          </div>
+        </div>
       </nav>
     </div>
   </header>
