a0a2f12c40ddc894553bb20c4ff677b3e9eef6e1
Tycho van Isterdael <t.vanisterdael@gmail.com>
2022-06-02 01:07:00 +0200

    het is laat ik zou gaan moeten slapen alleen maar domme fouten 2

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,7 +4,7 @@ import ActionView from '../views/ActionView.vue'
 import mainOidc from "@/oidc";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.VITE_APP_URL),
   routes: [
     {
       path: '/',
