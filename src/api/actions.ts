1c56d6a0bb6a60a80de448d4d405c8c4a5d3bd5a
Tycho van Isterdael <t.vanisterdael@gmail.com>
2022-06-02 00:53:51 +0200

    make staging kinda working 2

--- src/api/actions.ts
+++ src/api/actions.ts
@@ -5,7 +5,7 @@ export async function getActions(accessToken: string): Promise<Action[]> {
     let authHeader = "Bearer " + accessToken;
 
     return await axios
-        .get(import.meta.env.VITE_APP_URL + '/Action', { headers: {"Authorization": authHeader}})
+        .get("https://staging-actionservice.actionlogger.app/" + '/Action', { headers: {"Authorization": authHeader}})
         .then((res) => res.data)
         .catch(console.log)
 }
